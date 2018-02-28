c95cdcdad0a374cfc0da24929846dec5e48df728
kingtomat0 <simon-gabriel.fevre@laposte.net>
2018-02-28 14:48:52 +0100

    Test des points dynamiques sur la carte

--- app/src/main/java/com/example/alexandre/list/fragments/MapFragment.java
+++ app/src/main/java/com/example/alexandre/list/fragments/MapFragment.java
@@ -133,6 +133,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         // Add a marker in Sydney, Australia, and move the camera.
         LatLng castlePos = new LatLng(45.209299, 5.659144);
         LatLng treePos = new LatLng(MainActivity.posx, MainActivity.posy);
+
         CameraPosition liberty = CameraPosition.builder().target(castlePos).zoom(17)/*.bearing(0).tilt(0)*/.build();
         mGoogleMap.addMarker(new MarkerOptions().position(treePos).title("Test du cèdre du Liban").snippet("Libani"));
         mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(liberty));
